45983062b6a669b9dd01b63eb3141b911117ebd3
renato.goulart <renatotst55@outlook.com>
2020-11-11 11:21:15 -0300

    adiciona React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import "./index.css"
+import MainContent from "./componentes/MainContent"
+import Footer from "./componentes/Footer"
+import Header from "./componentes/Header"
+import Desktop from "./componentes/NotOnDesktop"
+import Login from "./componentes/Login"
+import "./login.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <div id="mainwrapper"></div>
+        <Header/>
+        <Login/>
+        <div id="content">
+          <Desktop/>
+          <MainContent />
+          <Footer />
+        </div>
+        <div id="footerbar">
+        </div>
+
     </div>
   );
 }
